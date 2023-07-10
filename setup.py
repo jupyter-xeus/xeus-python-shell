6cc45275b4f216a214424ec3aebaa7f1ad61192a
martinRenou <martin.renou@gmail.com>
2023-07-10 16:43:27 +0200

    Release 0.6.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ __AUTHOR__ = 'QuantStack dev team'
 
 setup(
     name='xeus-python-shell',
-    version='0.5.0',
+    version='0.6.0',
     description='The xeus-python core python logic.',
     author=__AUTHOR__,
     maintainer=__AUTHOR__,
